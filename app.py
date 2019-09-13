39c353fc5a8d5c0e67dce3de0c3c7dfc8db8651f
saherahmad96 <saherahmad96@gmail.com>
2019-09-12 22:59:01 -0400

    added routes

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
+SahersPortfolio = Flask(__name__)
 
-appname = Flask(__name__) 
+@SahersPortfolio.route('/SahersHome')
+def SahersHome():
+    return render_template('home.html', name='Saher')
 
-@appname.route('/')
-def homepage():
-    return jsonify("hey im saher")
+@SahersPortfolio.route('/NameData')
+def NameData():
+    names = {'Name':'Bob',
+    'Occupation':'Chicken'}
+    print(names{'Occupation'})
 
-if __name__ == "__main__":
-    appname.run(debug=True)
+    return jsonify(names)
+
+
+
+if __name__ == '__main__':
+    SahersPortfolio.run()
+
+    Lists = ['Mary', 'Kim', 'John']
+    print (Lists[0]) 
+
 
 
 
---
+++ test.py
@@ -0,0 +1,8 @@
+import app
+from app import SahersPortfolio
+
+
+if __name__ == '__main__':
+    print('testing')
+
+    SahersPortfolio.run()
