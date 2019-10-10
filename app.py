b945c76078109cf48537d596986b80e05c7097c0
saherahmad96 <saherahmad96@gmail.com>
2019-10-09 20:18:17 -0400

    App

--- app.py
+++ app.py
@@ -5,21 +5,29 @@ SahersPortfolio = Flask(__name__)
 def SahersHome():
     return render_template('home.html', name='Saher')
 
-@SahersPortfolio.route('/NameData')
-def NameData():
-    names = {'Name':'Bob',
-    'Occupation':'Chicken'}
-    print(names{'Occupation'})
+@SahersPortfolio.route('/BookData')
+def BookData():
+    book = {
+        "Pages": 344,
+        "Title": "The Great",
+        "Author": "JK Rowling",
+        "Genre": "Drama"     
+    }
 
-    return jsonify(names)
+    print(book["Author"])
+
+    return jsonify(book)
+
+@SahersPortfolio.route('/AboutMe')
+def AboutMe():
+    return render_template('aboutme.html', name='Saher', address='123 Chicken')
+
+
 
 
 
 if __name__ == '__main__':
     SahersPortfolio.run()
-
-    Lists = ['Mary', 'Kim', 'John']
-    print (Lists[0]) 
 
 
 
